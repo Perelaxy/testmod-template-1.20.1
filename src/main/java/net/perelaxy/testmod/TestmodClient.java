6338cbf3b824d73f11152ea5d1602db568190fd3
Perelaxy <tlandberg102@gmail.com>
2024-06-01 09:35:37 +0200

    Nothing

---
+++ src/main/java/net/perelaxy/testmod/TestmodClient.java
@@ -0,0 +1,10 @@
+package net.perelaxy.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
